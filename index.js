aad825f5e2f7016f4816387b825df5d65e75013e
victor poirier <poiriervictor99@gmail.com>
2020-02-13 22:40:38 +0000

    finished the ISSnode index.js !

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-/* const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
-
-let ipAdress = 'a';
-// let coordinates = { longitude: "-79.3716", latitude: "43.6319" };
-
-fetchMyIP((error, ip) => {
-  fetchCoordsByIP(ip, (error, coords) => {
-    fetchISSFlyOverTimes(coords, (error, flyOverTimes) => {
-      console.log(flyOverTimes);
-    });
-  });
-});
-
- */
-
 const { nextISSTimesForMyLocation } = require('./iss');
 
 nextISSTimesForMyLocation((error, passTimes) => {
---
+++ iss_promised.js
@@ -0,0 +1,30 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = () => {
+  return request('https://api.ipify.org/?format=json');
+}
+
+const fetchCoordsByIP = (body) => {
+  return request(`https://ipvigilante.com/${JSON.parse(body).ip}`);
+};
+
+const fetchISSFlyOverTimes = (body) => {
+  const { longitude, latitude } = JSON.parse(body).data;
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+}
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+
+module.exports = {
+  nextISSTimesForMyLocation
+}
